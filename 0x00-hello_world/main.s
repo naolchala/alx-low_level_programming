70c6fc5fe5fd22a1427a6f15ae9ed8e0e47e06d8
naolchala <naolchala6@gmail.com>
2021-06-17 17:16:37 +0300

    Intel

---
+++ 0x00-hello_world/main.s
@@ -0,0 +1,42 @@
+	.file	"main.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Hello World!"
+.LC1:
+	.string	"\n==============="
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	.LC1(%rip), %rdi
+	call	puts@PLT
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	$10, %edi
+	call	putchar@PLT
+	addl	$1, -4(%rbp)
+.L2:
+	cmpl	$9, -4(%rbp)
+	jle	.L3
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 10.2.1-6) 10.2.1 20210110"
+	.section	.note.GNU-stack,"",@progbits
