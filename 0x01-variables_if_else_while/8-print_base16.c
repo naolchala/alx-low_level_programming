f1a33cbbecdfbe6f9e634704758e8c5ed4cff309
naolchala <naolchala6@gmail.com>
2021-06-18 17:05:21 +0300

    Hexadecimal mandatory

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int num;
+char hex;
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+}
+for (hex = 'a'; hex <= 'f'; hex++)
+{
+putchar(hex);
+}
+putchar('\n');
+return (0);
+}
